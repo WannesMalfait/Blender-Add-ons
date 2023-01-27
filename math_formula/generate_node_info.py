072a805de5bb8d73d4e7742ada14d98244efa886
Wannes Malfait <wannes.malfait@gmail.com>
2023-01-27 19:05:56 +0100

    Improve node info generation
    - Handle some special characters like "&"
    - Sort the generated lists to make the git diffs more logical.
    - Check for invalid aliases at the end.
    - Improve the generation of all property possibilities.

--- math_formula/generate_node_info.py
+++ math_formula/generate_node_info.py
@@ -36,7 +36,9 @@ def generate_node_info():
 
     def snake(s: str) -> str:
         """Turn a string into snake case"""
-        return '_'.join(s.split()).replace('/', '_').replace('-', '_').lower()
+        first_try = '_'.join(s.split()).replace(
+            '/', '_').replace('-', '_').replace('&', '_').lower()
+        return ''.join([c for c in first_try if c.isalpha() or c.isnumeric() or c == '_'])
 
     shader_geo_alias_strs = []
     geometry_alias_strs = []
@@ -69,12 +71,7 @@ def generate_node_info():
 
         # Generate aliases and validate properties
         default_name = snake(node.bl_label)
-        name_valid = True
-        for c in default_name:
-            if not (c.isalpha() or c.isnumeric() or c == '_'):
-                name_valid = False
-                break
-        if not name_valid:
+        if 'legacy' in default_name:
             continue
         curr_state = [0 for _ in props]
 
@@ -108,13 +105,16 @@ def generate_node_info():
                 prop = props[prop_i]
                 enum_value = prop.enum_items[enum_j]
                 try:
+                    prev_value = getattr(node, prop.identifier)
                     setattr(node, prop.identifier,
                             enum_value.identifier)
                 except:
                     # This property can't actually be set.
+                    # TODO: Check other permutations of setting this property.
                     continue
                 # TODO: check that this is a valid name (no '&,' '/'...)
                 rec(prop_i+1, name + '_' + snake(enum_value.name))
+                setattr(node, prop.identifier, prev_value)
 
         # Generate the default case as well.
         if len(props) > 0:
@@ -140,6 +140,10 @@ def generate_node_info():
         end_marker = '# End auto generated\n'
         start = text.find(start_marker) + len(start_marker)
         end = text.find(end_marker)
+        builtin_node_strs.sort()
+        shader_alias_strs.sort()
+        shader_geo_alias_strs.sort()
+        geometry_alias_strs.sort()
         builtin_node_str = '\n\t'.join(builtin_node_strs)
         shader_geo_alias_str = '\n\t'.join(shader_geo_alias_strs)
         geometry_alias_str = '\n\t'.join(geometry_alias_strs)
@@ -171,9 +175,21 @@ shader_node_aliases = {{
         f.truncate(len(new_text))
 
     import importlib
-    from .backends import builtin_nodes
+    from .backends import builtin_nodes, geometry_nodes, shader_nodes
     importlib.reload(builtin_nodes)
-    # TODO: check that all aliases are well-defined
+    importlib.reload(geometry_nodes)
+    importlib.reload(shader_nodes)
+    for options_dict in [builtin_nodes.instances, geometry_nodes.geometry_nodes, shader_nodes.shader_nodes]:
+        for options in options_dict.values():
+            for node_name in options:
+                if isinstance(node_name, str):
+                    if node_name in builtin_nodes.geometry_node_aliases:
+                        continue
+                    if node_name in builtin_nodes.shader_node_aliases:
+                        continue
+                    if node_name in builtin_nodes.shader_geo_node_aliases:
+                        continue
+                    print('Invalid alias:', node_name)
 
 
 bpy.app.timers.register(generate_node_info, first_interval=0)
